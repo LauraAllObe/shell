267a1bf5c3166c6f26ef7b82f9deb469744f08a0
aharp097 <114882973+aharp097@users.noreply.github.com>
2023-10-08 22:59:13 -0400

    Update mytimeout.c

--- src/mytimeout/mytimeout.c
+++ src/mytimeout/mytimeout.c
@@ -10,6 +10,34 @@
 //global variable for child process pt10
 pid_t child_pid = 0;
 //part 10
+//grab path
+char* get_path(char *cmd)
+{
+	char *path = getenv("PATH");
+	if(!path)
+	{
+		fprintf(stderr, "PATH environment variable not set.\n");
+		return NULL;
+	}
+	
+	char *pathCopy = strdup(path);
+	char *dir = strtok(pathCopy, ":");
+
+	while (dir!=NULL)
+	{
+		char executable[512];
+		snprintf(executable, sizeof(executable), "%s/%s", dir, cmd);
+		if (access(executable, F_OK == 0 && access(executable, X_OK) == 0))
+		{
+			free(pathCopy);
+			return strdup(executable);
+		}
+		dir = strtok(NULL, ":");
+	}
+
+	free(pathCopy);
+	return NULL;
+}
 /*signal handler for the alarm signal. This handler will be triggered when a process receives an SIGALRM
  signal, which will be sent when the timer we'll set (with alarm()) expires.*/
 void alarm_handler(int signum) {
@@ -19,14 +47,15 @@ void alarm_handler(int signum) {
 }
 
 int main(int argc, char *argv[]){
+//printf("%d", argc);
     if (argc < 3) {
         printf("Usage: ./mytimeout [snds] [cmd] [cmd-args].\n");
         exit(1);
     }
-    
+ //printf("%d", argc);
     int duration = atoi(argv[1]);
 
-    char **exec_argv = (char **)malloc((argc+1) * sizeof(char *));	//get arguments
+    char **exec_argv = (char **)malloc((argc-2) * sizeof(char *));	//get arguments, starts grab at argv[2]
 
     if (exec_argv == NULL)
     {
@@ -34,13 +63,53 @@ int main(int argc, char *argv[]){
 	exit(1);
     }
 
-    for (int i = 0; i < argc; i++)
+    for (int i = 0; i < argc-2; i++)
     {
-	exec_argv[i] = argv[i];
+    	exec_argv[i] = NULL;
     }
+    for (int i = 2; i < argc; i++)
+    {
+	exec_argv[i-2] = argv[i];
+    }
+    exec_argv[argc-2] = NULL;
+	/*for(int i = 0; i < argc-2; i++)
+		{
+			printf("%s", exec_argv[i]);
+		}
+printf("\n");
+    char *cmd = exec_argv[0];	//OLD PATH GRAB, BARELY WORKS
+    char pathstr[1024];
+    int pathCount = 0;
+    char paths[100][256];
+    FILE *path = popen(cmd, "r");
+    if (path == NULL) 
+    {
+        perror("popen");
+        return 1;
+    }
+    
+    while (fgets(pathstr, sizeof(pathstr), path) != NULL) 
+    {
+        // Remove trailing newline
+        if (pathstr[strlen(pathstr) - 1] == '\n') {
+            pathstr[strlen(pathstr) - 1] = '\0';
+        }
+	strncpy(paths[pathCount], pathstr, sizeof(paths[pathCount]));
+        pathCount++;
 
-    exec_argv[argc] = NULL;
+        // Check if we've reached the maximum number of paths
+        if (pathCount >= 100) {
+            printf("Maximum number of paths reached.\n");
+            break;
+        }
+    }*/
 
+    char* fullPath = get_path(argv[2]);
+    if(!fullPath)
+    {
+	perror("Command not found/not executable");
+    }
+	//printf("%s", fullPath);
     pid_t pid = fork();
 
     if(pid == -1){
@@ -50,11 +119,13 @@ int main(int argc, char *argv[]){
 
     if(pid == 0){
         //execute command
-
-        execv(exec_argv[2], exec_argv);
-        //if execv fails
-        perror("exec");
-        exit(1);
+	/*for (int i = 0; i < pathCount; i++) {		//halfway works w/ OLD PATH GRAB
+        char *exec_argv[] = {paths[i], NULL};
+        if (execv(paths[i], exec_argv) == -1) {
+            perror("execv");
+        }*/
+       execv(fullPath, exec_argv);
+    
         
     }else{
         //part 10. this allows to capture the SIGALRM signal whenever an alarm goes off
